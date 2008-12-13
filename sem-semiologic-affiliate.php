176d274f35eb1543ab6c98f7083ee8e3d0acf7ad
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-13 15:50:03 +0000

    change author URIs
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-semiologic-affiliate/trunk@71 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-semiologic-affiliate.php
+++ sem-semiologic-affiliate.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.semiologic.com/software/marketing/sem-affiliate/
 Description: Automatically adds your affiliate ID to all links to Semiologic.
 Author: Denis de Bernardy
 Version: 1.7
-Author URI: http://www.semiologic.com
+Author URI: http://www.getsemiologic.com
 Update Service: http://version.semiologic.com/wordpress
 Update Tag: semiologic_affiliate
 Update Package: http://www.semiologic.com/media/software/marketing/sem-affiliate/sem-semiologic-affiliate.zip
