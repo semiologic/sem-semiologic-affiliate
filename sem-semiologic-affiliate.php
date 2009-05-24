fa78f518723fb0d9237d515a7701f69fac12a7ea
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-05-24 16:03:42 +0000

    good to go
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/sem-semiologic-affiliate/trunk@1514 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- sem-semiologic-affiliate.php
+++ sem-semiologic-affiliate.php
@@ -3,7 +3,7 @@
 Plugin Name: Semiologic Affiliate
 Plugin URI: http://www.semiologic.com/software/sem-affiliate/
 Description: Automatically adds your affiliate ID to all links to Semiologic.
-Version: 1.9 beta
+Version: 2.0 RC
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Text Domain: sem-semiologic-affiliate-info
@@ -35,11 +35,13 @@ if ( !defined('semiologic_affiliate_debug') )
 add_action('admin_menu', array('semiologic_affiliate', 'admin_menu'));
 
 if ( !is_admin() && semiologic_affiliate::get_campaign() ) {
+	if ( !class_exists('anchor_utils') )
+		include dirname(__FILE__) . '/anchor-utils/anchor-utils.php';
+	
 	if ( !semiologic_affiliate_debug ) {
-		add_action('wp_head', array('semiologic_affiliate', 'ob_start'), 1000);
+		add_filter('ob_filter_anchor', array('semiologic_affiliate', 'filter'));
 	} else {
-		add_filter('the_content', array('semiologic_affiliate', 'filter'));
-		add_filter('comment_text', array('semiologic_affiliate', 'filter'));
+		add_filter('filter_anchor', array('semiologic_affiliate', 'filter'));
 	}
 }
 
@@ -100,51 +102,21 @@ class semiologic_affiliate {
 	
 	
 	/**
-	 * ob_start()
-	 *
-	 * @return void
-	 **/
-
-	function ob_start() {
-		ob_start(array('semiologic_affiliate', 'filter'));
-	} # ob_start()
-	
-	
-	/**
 	 * filter()
 	 *
-	 * @param string $buffer
-	 * @return string $buffer
+	 * @param array $anchor
+	 * @return array $anchor
 	 **/
 
-	function filter($str) {
-		return preg_replace_callback("/<a\s+(.*?)>/ix", array('semiologic_affiliate', 'filter_callback'), $str);
+	function filter($anchor) {
+		if ( !preg_match("/^https?:\/\/(?:www\.)?(?:get)?semiologic\.com\b/", $anchor['attr']['href']) )
+			return $anchor;
+		
+		if ( strpos($anchor['attr']['href'], '?') === false )
+			$anchor['attr']['href'] .= '?aff=' . urlencode(semiologic_affiliate::get_campaign());
+		
+		return $anchor;
 	} # filter()
-	
-	
-	/**
-	 * filter_callback()
-	 *
-	 * @param array $match
-	 * @return string $str
-	 **/
-
-	function filter_callback($match) {
-		preg_match("/\bhref=([\"'])(.+?)\\1/i", $match[1], $href);
-		
-		if ( !preg_match("/\b(?:get)?semiologic\.com\b/i", $href[0]) )
-			return $match[0];
-		
-		$raw_href = current($href);
-		$new_href = end($href);
-		
-		$campaign_key = semiologic_affiliate::get_campaign();
-		
-		if ( strpos($new_href, '?') === false )
-			$new_href .= '?aff=' . urlencode($campaign_key);
-		
-		return str_replace($raw_href, 'href="' . $new_href . '"', $match[0]);
-	} # filter_callback()
 } # semiologic_affiliate
 
 
